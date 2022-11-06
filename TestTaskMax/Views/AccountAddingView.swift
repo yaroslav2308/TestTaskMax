7dfec9bff5b10f41cc9f3931c0d12efc6fd7f548
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-11-06 14:29:38 +0300

    Add necessary assets and set file structure

---
+++ TestTaskMax/Views/AccountAddingView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountAddingView.swift
+//  TestTaskMax
+//
+//  Created by Yaroslav Monastyrev on 06.11.2022.
+//
+
+import SwiftUI
+
+struct AccountAddingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountAddingView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountAddingView()
+    }
+}
