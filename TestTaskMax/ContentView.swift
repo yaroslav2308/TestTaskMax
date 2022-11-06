85c6931ab319246b8f5f869491ab9e011d8427d6
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-11-06 14:09:10 +0300

    Initial Commit

---
+++ TestTaskMax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestTaskMax
+//
+//  Created by Yaroslav Monastyrev on 06.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestTaskMax/TestTaskMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskMaxApp.swift
+//  TestTaskMax
+//
+//  Created by Yaroslav Monastyrev on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
