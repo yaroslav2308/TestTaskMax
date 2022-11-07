ed618e6f0aef733e692c2b37723c6f4a1b9b96b0
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-11-07 19:41:06 +0300

    Edit viewModel

--- TestTaskMax/ViewModel/UserDataViewModel.swift
+++ TestTaskMax/ViewModel/UserDataViewModel.swift
@@ -8,18 +8,6 @@
 import Foundation
 
 class UserDataViewModel: ObservableObject {
-//    @Published var name: String = ""
-//    @Published var mail: String = ""
-//    @Published var password: String = ""
-//    @Published var incomingServer: String = ""
-//    @Published var outgoingServer: String = ""
-//    @Published var incomingPort: IncomingPort = .port143
-//    @Published var outgoingPort: OutgoingPort = .port25
-//    @Published var incomingSecurity: SecurityProtocolCases = .withoutSecurity
-//    @Published var outgoingSecurity: SecurityProtocolCases = .withoutSecurity
-//    @Published var insecureIncomingSSL: Bool = false
-//    @Published var insecureOutgoingSSL: Bool = false
-//    @Published var userFolder: String = ""
     @Published var userData: UserData = UserData() {
         // save user data every time userData property is updated
         didSet {
